d7f453bee53e10926d212ef6fda80c6888bef68d
sdxsd <willmaguire@icloud.com>
2023-08-23 12:40:04 +0200

    Error handling.

--- ex00/main.cpp
+++ ex00/main.cpp
@@ -1,10 +1,20 @@
 #include "BitcoinExchange.hpp"
 
 int main(int argc, char *argv[]) {
-	if (argc != 2)
+	if (argc != 2) {
 		std::cout << "Invalid arguments to program." << std::endl;
+		return (1);
+	}
 	std::ifstream dbFile("data.csv");
 	std::ifstream inputFile(argv[1]);
+	if (dbFile.fail()) {
+		std::cerr << "Failed to open database file." << std::endl;
+		return (1);
+	}
+	else if (inputFile.fail()) {
+		std::cerr << "Failed to open input file." << std::endl;
+		return (1);
+	}
 	BitcoinExchange exchange(dbFile);
 	exchange.parseInput(inputFile);
 }
