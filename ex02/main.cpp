784c31feb60d56f73d8bf839deba045f050fc5dc
sdxsd <willmaguire@icloud.com>
2023-12-06 14:28:13 +0100

    More useful test.

--- ex02/main.cpp
+++ ex02/main.cpp
@@ -2,10 +2,10 @@
 #include <iostream>
 
 int main(void) {
-	std::vector<int> list = {12, 2};
+	std::vector<int> list = {12, 2, 53, 3, 98, 76, 200, 190};
 
 	std::vector<int> sortedList = insertionSort(list);
 	for (unsigned long i = 0; i < sortedList.size(); i++)
-		std::cout << sortedList[i] << " ";
+		std::cout << sortedList[i] << std::endl;
 	std::cout << std::endl;
 }
